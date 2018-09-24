9ba04c21d976d0440ff59fa2b4f916c11dbbb7a4
Matthias Malzahn <matthias.malzahn@comlineag.de>
2018-09-24 15:47:16 +0200

    add baseinfra files

---
+++ getbaseinfrastruktur/variables.tf
@@ -0,0 +1,9 @@
+variable "remote_state_bucket_region" {
+  default     = "eu-west-1"
+}
+variable "remote_state_bucket" {
+  default = "tf-state-ayhynmmqenhbbtjy"
+}
+variable "remote_state_key" {
+  default = "baseinfrastruktur.state"
+}
